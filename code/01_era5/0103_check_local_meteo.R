e921bbeec4f379c2bb0b3894d50f0880e408e806
Thomas Zieher <thomas.zieher@bfw.gv.at>
2025-09-11 01:36:58 +0200

    grass run

--- code/01_era5/0103_check_local_meteo.R
+++ code/01_era5/0103_check_local_meteo.R
@@ -49,6 +49,17 @@ data=read.table("MeteoCR2019-2024_rep.txt",sep="\t",header=T)#,skip=1)
 ##add date
 data["date"]=as.Date(sprintf("%i-%0.3i",as.numeric(data$Year),as.numeric(data$Day)),format="%Y-%j")
 
+
+##check nas
+aggregate(is.na(data$Rain),by=list(format(data$date,format="%m")),FUN="sum")
+aggregate(is.na(data$Rain),by=list(format(data$date,format="%Y")),FUN="sum")
+aggregate(is.na(data$Rain),by=list(format(data$date,format="%j")),FUN="sum")
+
+barplot(table(data$Month))
+barplot(table(data$Year))
+
+
+
 ##aggregate variables
 data_tmean=aggregate(data$T,by=list(data$date),FUN="mean",na.rm=T)
 names(data_tmean)=c("date","tmean")
@@ -75,28 +86,37 @@ data_daily=merge(data_daily,data_globrad,by.x="date",by.y="date")
 data_daily=merge(data_daily,data_wind,by.x="date",by.y="date")
 data_daily=merge(data_daily,data_rh,by.x="date",by.y="date")
 
+barplot(table(format(data_daily$date,format="%Y")))
+barplot(table(format(data_daily$date,format="%m")))
+
+
+
 ##fill gaps
 data_daily_filled=data.frame(date=data_daily$date)
 
-# ##kalman filter
-# data_daily_filled$tmean=imputeTS::na_kalman(data_daily$tmean)
-# data_daily_filled$tmin=imputeTS::na_kalman(data_daily$tmin)
-# data_daily_filled$tmax=imputeTS::na_kalman(data_daily$tmax)
-# data_daily_filled$prec=imputeTS::na_kalman(data_daily$prec)
-# data_daily_filled$globrad=imputeTS::na_kalman(data_daily$globrad)
-# data_daily_filled$wind=imputeTS::na_kalman(data_daily$wind)
-# data_daily_filled$rh=imputeTS::na_kalman(data_daily$rh)
+##kalman filter
+data_daily_filled$tmean=imputeTS::na_kalman(data_daily$tmean)
+data_daily_filled$tmin=imputeTS::na_kalman(data_daily$tmin)
+data_daily_filled$tmax=imputeTS::na_kalman(data_daily$tmax)
+data_daily_filled$prec=imputeTS::na_kalman(data_daily$prec)
+data_daily_filled$globrad=imputeTS::na_kalman(data_daily$globrad)
+data_daily_filled$wind=imputeTS::na_kalman(data_daily$wind)
+data_daily_filled$rh=imputeTS::na_kalman(data_daily$rh)
 
-##interpolation
-data_daily_filled$tmean=imputeTS::na_interpolation(data_daily$tmean)
-data_daily_filled$tmin=imputeTS::na_interpolation(data_daily$tmin)
-data_daily_filled$tmax=imputeTS::na_interpolation(data_daily$tmax)
-data_daily_filled$prec=imputeTS::na_interpolation(data_daily$prec)
-data_daily_filled$globrad=imputeTS::na_interpolation(data_daily$globrad)
-data_daily_filled$wind=imputeTS::na_interpolation(data_daily$wind)
-data_daily_filled$rh=imputeTS::na_interpolation(data_daily$rh)
+# ##interpolation
+# data_daily_filled$tmean=imputeTS::na_interpolation(data_daily$tmean)
+# data_daily_filled$tmin=imputeTS::na_interpolation(data_daily$tmin)
+# data_daily_filled$tmax=imputeTS::na_interpolation(data_daily$tmax)
+# data_daily_filled$prec=imputeTS::na_interpolation(data_daily$prec)
+# data_daily_filled$globrad=imputeTS::na_interpolation(data_daily$globrad)
+# data_daily_filled$wind=imputeTS::na_interpolation(data_daily$wind)
+# data_daily_filled$rh=imputeTS::na_interpolation(data_daily$rh)
+
 
 any(is.na(data_daily_filled))
+barplot(table(format(data_daily_filled$date,format="%Y")))
+barplot(table(format(data_daily_filled$date,format="%m")))
+
 
 
 ##function for saturation vapour pressure (Tetens formula)
@@ -121,7 +141,32 @@ meteo$vappres=ea_from_T_RH(T=data_daily_filled$tmean,RH=data_daily_filled$rh)
 meteo$windspeed=data_daily_filled$wind
 
 
+barplot(table(format(meteo$date,format="%Y")))
+barplot(table(format(meteo$date,format="%m")))
 
-write.table(meteo,"meteo_daily_local.csv",sep=";",row.names=F,quote=F)
+
+##Walther-Lieth diagram
+start_date=as.Date("2019-01-01")
+end_date=as.Date("2024-12-31")
+##aggregate to monthly values
+tmin=setDT(meteo)[year(dates)%in%year(start_date):year(end_date), .(tmin=min(tmin)),by=.(yr=year(dates),mon=month(dates))]
+tmax=setDT(meteo)[year(dates)%in%year(start_date):year(end_date), .(tmax=max(tmax)),by=.(yr=year(dates),mon=month(dates))]
+psum=setDT(meteo)[year(dates)%in%year(start_date):year(end_date), .(psum=sum(prec)),by=.(yr=year(dates),mon=month(dates))]
+
+##aggregate to mean monthly values
+meteo_monthly=aggregate(data.frame(psum=psum$psum,
+                                   tmax=tmax$tmax,
+                                   tmin=tmin$tmin,
+                                   tabsmin=abs(tmin$tmin)),
+                        by=list(tmin$mon),FUN="mean")
+
+t(meteo_monthly[,2:5])
+
+
+par(mfrow=c(1,1))
+diagwl(t(meteo_monthly[,2:5]), est="Location",alt="XXX",mlab="en",cols=NULL)
+
+
+write.table(meteo,"meteo_local.csv",sep=";",row.names=F,quote=F)
 
 
